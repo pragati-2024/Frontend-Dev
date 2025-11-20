361202a4257e0ce3d3f582ab591eb63cd5b05f76
pragati-2024 <bpragati21@gmail.com>
2025-11-20 12:50:08 +0530

    Add ApplyOperation.js - applyOperation function implementation

---
+++ ApplyOperation.js
@@ -0,0 +1,16 @@
+function applyOperation(numbers, operation) {
+    const result = [];
+    for (let num of numbers) {
+        result.push(operation(num));  // applying callback to each number
+    }
+    return result;
+}
+
+// Callback functions
+const double = (n) => n * 2;
+const square = (n) => n * n;
+
+const nums = [1, 2, 3, 4];
+
+console.log("Double:", applyOperation(nums, double));
+console.log("Square:", applyOperation(nums, square));
